719852f05b9e64ef693c5773076216824c62b319
milk-maid <roqbell@gmail.com>
2023-10-05 11:53:27 +0100

    Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
    use the function puts
    
    you are not allowed to use printf
    
    your program should end with the valu

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print the phrase: "Programming is like building a multilingual puzzle
+ * Return: 0 if exiter properly.. non-zero if otherwise
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
